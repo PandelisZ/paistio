8b7e18fe5133b41c9c9def1c3c545c51a3ec7dfb
Pandelis Zembashis <info@pandelis.me>
2016-04-23 12:19:26 +0100

    basic plugin

---
+++ spec/footsize-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FootsizeView from '../lib/footsize-view';
+
+describe('FootsizeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
